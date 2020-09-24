f67965a325b9aef04e589b6bdb30de9039e98359
ahmadsupri <supri@comeapp.id>
2020-09-25 02:52:44 +0700

    about section in landing page

---
+++ resources/views/layouts/home/about.blade.php
@@ -0,0 +1,17 @@
+<div class="about">
+    <div class="about__container">
+        <div class="about__content">
+            <h3 class="about__title">Ayo Bantu Penjahit Indonesia 
+Industri Fashion Dalam Negeri</h3>
+            <p class="about__text">JAHIT.CO.ID lahir pada tahun 2020 di era pandemic COVID 19, Kami mempunyai semangat untuk meningkatkan Industri textile dan fashion dalam negeri. Kami membantu para penjahit yang sudah kami verifikasi menjadi bagian dari partner kami untuk mendapatkan customer lebih luas dan lebih cepat melalui platform kami. Pelanggan akan langsung mendapatkan balasan dari jaringan penjahit kami lebih cepat mudah dan terjangkau.</p>
+        </div>
+        <div class="about__media">
+            <div class="about__wrapper">
+                <div class="media-preview">
+                    <!-- <img src="/img/media-preview.png" alt="media-preview" /> -->
+                    <iframe class="media-preview__video" title="video" src="https://www.youtube.com/embed/kJQP7kiw5Fk?controls=0&disablekb=1&fs=0&loop=1&modestbranding=1&playsinline=1"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

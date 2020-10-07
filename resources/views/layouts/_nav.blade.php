59a9679896b529d7774a0986c25a89c2963417bd
razaqa <razaqa.haffiyan@amartha.com>
2020-10-08 04:36:20 +0700

    Add link to navbar daftar and masuk

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -5,8 +5,26 @@
             <div class="navbar__logo"></div>
         </div>
         <div class="navbar__auth">
-            <a href="/user/register">Daftar</a>
-            <a href="/user/login"><button class="btn btn-outline-light">Masuk</button></a>
+        @guest
+            <a href="{{ route('register') }}">Daftar</a>
+            <a href="{{ route('login') }}"><button class="btn btn-outline-light">Masuk</button></a>
+        @else
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+        @endguest
         </div>
     </div>
     <div id="expand-content-nav"></div>

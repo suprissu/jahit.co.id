d75f1e59162f624bb8f8d38b15c1746aa662663d
butterchoco <ahmadsupri1999@gmail.com>
2020-10-12 09:44:04 +0700

    fix style admin dashboard

--- resources/views/pages/administrator/dashboard.blade.php
+++ resources/views/pages/administrator/dashboard.blade.php
@@ -9,102 +9,126 @@
 @endsection
 
 @section('extra-css')
-
+<style>
+    .admin__dashboard {
+        margin: 12vh 0;
+    }
+</style>
 @endsection
 
 @section('content')
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <h1>Hi! I'm {{ $role }}</h1>
-    
-    <hr>
-    <h3>{{ $customers->count() }} Customers</h3>
-    <div class="ml-5">
-    @foreach ($customers as $counter=>$customer)
-        <hr>
-        <div>[{{ $counter + 1 }}] {{ $customer->user->name }}</div>
-        <div>Email : {{ $customer->user->email }}</div>
-        <div>Perusahaan : {{ $customer->company_name }}</div>
-        <div>Nomor Telepon : {{ $customer->phone_number }}</div>
-        @if ($customer->user->is_active == 1)
-            <div>Status : Aktif</div>
-        @else
-            <div>Status : Tidak Aktif</div>
-        @endif
-        <div>Pesanan :</div>
-        <div class="ml-5">
-        @foreach ($customer->projects as $index => $project)
-            <hr>
-            <div>[{{ $index + 1 }}] {{ $project->name }}</div>
-            <div>Kategori : {{ $project->category->name }}</div>
-            <div>Jumlah : {{ $project->count }}</div>
-            <div>Alamat : {{ $project->address }}</div>
-            <div>Catatan : {{ $project->note }}</div>
-            <div>Desain :</div>
-            @foreach ($project->images as $image)
-                <img width="100px" src="{{ asset($image->path) }}">
-            @endforeach
-            <br>
-        @endforeach
-        <br>
+<div class="admin__dashboard">
+    <div class="admin__dashboard___container container">
+        <h1 class="mb-3">Hi, {{ $role }}</h1>
+        <div class="row">
+            <div class="col-sm-12 col-md-8">
+                <div class="admin__dashboard__header list-group" id="list-tab" role="tablist">
+                        <a class="list-group-item list-group-item-action active" id="list-customer-list" data-toggle="list" href="#list-customer" role="tab" aria-controls="customer">Customers ({{ $customers->count() }})</a>
+                        <a class="list-group-item list-group-item-action" id="list-partner-list" data-toggle="list" href="#list-partner" role="tab" aria-controls="partner">Partners ({{ $partners->count() }})</a>
+                        <a class="list-group-item list-group-item-action" id="list-project-list" data-toggle="list" href="#list-project" role="tab" aria-controls="project">Projects ({{ $projects->count() }})</a>
+                </div>
+                <div class="admin__dashboard__list header tab-content" id="nav-tabContent">
+                    
+                    <!-- Semua Proyek -->
+                    <div class="tab-pane fade show active" id="list-customer" role="tabpanel" aria-labelledby="list-customer-list">
+                        <!-- TODO: Make List Item -->
+                        @foreach ($customers as $counter=>$customer)
+                            <ul class="list-group my-3">
+                                <li class="list-group-item">
+                                    <h4 class="mb-4">{{ $customer->user->name }}</h4>
+                                    <p><strong>Email :</strong><br/> {{ $customer->user->email }}</p>
+                                    <p><strong>Perusahaan :</strong><br/> {{ $customer->company_name }}</p>
+                                    <p><strong>Nomor Telepon :</strong><br/> {{ $customer->phone_number }}</p>
+                                    @if ($customer->user->is_active == 1)
+                                        <p><strong>Status :</strong><br/> Aktif</p>
+                                    @else
+                                        <p><strong>Status :</strong><br/> Tidak Aktif</p>
+                                    @endif
+                                </li>
+                                <li class="list-group-item btn btn-outline-danger" data-toggle="collapse" data-target="#order-{{ $counter }}" aria-expanded="false" aria-controls="#order-{{ $counter }}"><strong>Pesanan</strong></li>
+                                <li class="list-group-item collapse" id="order-{{ $counter }}">
+                                    @foreach ($customer->projects as $index => $project)
+                                        <div class="card card-body my-3">
+                                            <h4 class="mb-4">{{ $project->name }}</h4>
+                                            <p><strong>Kategori :</strong><br/> {{ $project->category->name }}</p>
+                                            <p><strong>Jumlah :</strong><br/> {{ $project->count }}</p>
+                                            <p><strong>Alamat :</strong><br/> {{ $project->address }}</p>
+                                            <p><strong>Catatan :</strong><br/> {{ $project->note }}</p>
+                                            <p><strong>Desain :</strong><br/></p>
+                                            <div class="row mx-2">
+                                                @foreach ($project->images as $image)
+                                                    <img class="border rounded mx-1" height="100" width="100" src="{{ asset($image->path) }}" alt="image-preview">
+                                                @endforeach
+                                            </div>
+                                            <br>
+                                        </div>
+                                    @endforeach
+                                </li>
+                            </ul>
+                        @endforeach
+                    </div>
+                    
+                    <!-- Penawaran Terbuka -->
+                    <div class="tab-pane fade" id="list-partner" role="tabpanel" aria-labelledby="list-partner-list">
+                        @foreach ($partners as $counter=>$partner)
+                            <div class="card my-3">
+                                <div class="card-body">
+                                    <h4 class="mb-4">{{ $partner->user->name }}</h4>
+                                    <p><strong>Email :</strong><br/> {{ $partner->user->email }}</p>
+                                    <p><strong>Perusahaan :</strong><br/> {{ $partner->company_name }}</p>
+                                    <p><strong>Nomor Telepon :</strong><br/> {{ $partner->phone_number }}</p>
+                                    @if ($partner->user->is_active == 1)
+                                        <p><strong>Status :</strong><br/> Aktif</p>
+                                    @else
+                                        <p><strong>Status :</strong><br/> Tidak Aktif</p>
+                                    @endif
+                                </div>
+                                <div class="row m-2">
+                                    <div class="col">
+                                        <p><strong>KTP :</strong><br/></p>
+                                        <img class="border rounded img-fluid img-thumbnail" src="{{ asset($partner->ktp_pict_link) }}" alt="ktp_image">
+                                    </div>
+                                    <div class="col">
+                                        <p><strong>NPWP :</strong><br/></p>
+                                        <img class="border rounded img-fluid img-thumbnail" src="{{ asset($partner->npwp_pict_link) }}" alt="npwp_image">
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                    
+                    <!-- Proyek Dalam Pengerjaan -->
+                    <div class="tab-pane fade" id="list-project" role="tabpanel" aria-labelledby="list-project-list">
+                        <!-- TODO: Make List Item -->
+                        @foreach ($projects as $index => $project)
+                            <div class="card card-body my-3">
+                                <h4 class="mb-4">{{ $project->name }}</h4>
+                                <p><strong>Kategori :</strong><br/> {{ $project->category->name }}</p>
+                                <p><strong>Jumlah :</strong><br/> {{ $project->count }}</p>
+                                <p><strong>Alamat :</strong><br/> {{ $project->address }}</p>
+                                <p><strong>Catatan :</strong><br/> {{ $project->note }}</p>
+                                <p><strong>Desain :</strong><br/></p>
+                                <div class="row mx-2">
+                                    @foreach ($project->images as $image)
+                                        <img class="border rounded mx-1" height="100" width="100" src="{{ asset($image->path) }}" alt="image-preview">
+                                    @endforeach
+                                </div>
+                                <br>
+                            </div>
+                    @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <h3>{{ $categories->count() }} Categories</h3>
+                <div class="list-group">
+                    @foreach ($categories as $index => $category)
+                        <div class="list-group-item btn btn-outline-dark">{{ $category->name}}</div>
+                    @endforeach
+                </div>
+            </div>
         </div>
-    @endforeach
-    </div>
-    
-    <hr>
-    <h3>{{ $partners->count() }} Partners</h3>
-    <div class="ml-5">
-    @foreach ($partners as $counter=>$partner)
-        <hr>
-        <div>[{{ $counter + 1 }}] {{ $partner->user->name }}</div>
-        <div>Email : {{ $partner->user->email }}</div>
-        <div>Perusahaan : {{ $partner->company_name }}</div>
-        <div>Nomor Telepon : {{ $partner->phone_number }}</div>
-        @if ($partner->user->is_active == 1)
-            <div>Status : Aktif</div>
-        @else
-            <div>Status : Tidak Aktif</div>
-        @endif
-        <div>KTP :</div>
-        <img width="100px" src="{{ asset($partner->ktp_pict_link) }}">
-        <div>NPWP :</div>
-        <img width="100px" src="{{ asset($partner->npwp_pict_link) }}">
-    @endforeach
-    </div>
-    
-    <hr>
-    <h3>{{ $projects->count() }} Projects</h3>
-    <div class="ml-5">
-        @foreach ($projects as $index => $project)
-            <hr>
-            <div>[{{ $index + 1 }}] {{ $project->name }}</div>
-            <div>Kategori : {{ $project->category->name }}</div>
-            <div>Jumlah : {{ $project->count }}</div>
-            <div>Alamat : {{ $project->address }}</div>
-            <div>Catatan : {{ $project->note }}</div>
-            <div>Desain :</div>
-            @foreach ($project->images as $image)
-                <img width="100px" src="{{ asset($image->path) }}">
-            @endforeach
-            <br>
-        @endforeach
-    </div>
-
-    <hr>
-    <h3>{{ $categories->count() }} Categories</h3>
-    <div class="ml-5">
-        @foreach ($categories as $index => $category)
-            <hr>
-            <div>[{{ $index + 1 }}] {{ $category->name}}</div>
-        @endforeach
-    </div>
+</div>
 @endsection
 
 @section('extra-js')

1e06a597be28177f0fce8c1c9ebab54026e21819
razaqa <razaqa.haffiyan@amartha.com>
2020-10-13 02:04:12 +0700

    Fix routing in template

--- resources/views/layouts/home/partnerInvitation.blade.php
+++ resources/views/layouts/home/partnerInvitation.blade.php
@@ -3,6 +3,6 @@
         <h3 class="partnerInvitation__title">Kamu Penjahit Konveksi ?</h3>
         <p class="partnerInvitation__description">Dapatkan proyek jahitan langsung di JAHIT.CO.ID. Kami juga akan menyediakan modal kerja untuk proyek yang sedang dijalani tanpa bunga.</p>
         <p class="partnerInvitation__invitation">Ayo gabung bersama jahit.co.id !</p>
-        <button class="btn btn-outline-danger">Daftar menjadi mitra jahit.co.id</button>
+        <a href="{{ route('register') }}" class="btn btn-outline-danger">Daftar menjadi mitra jahit.co.id</a>
     </div>
 </div>

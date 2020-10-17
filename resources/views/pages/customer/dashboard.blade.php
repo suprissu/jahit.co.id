c6e1dd2949f4ed34241a86bfedd1ed0b87ac014e
butterchoco <ahmadsupri1999@gmail.com>
2020-10-17 15:36:08 +0700

    Fix transaction list item

--- resources/views/pages/customer/dashboard.blade.php
+++ resources/views/pages/customer/dashboard.blade.php
@@ -37,7 +37,90 @@
                 
                 <!-- Semua Proyek -->
                 <div class="tab-pane fade show active" id="list-all" role="tabpanel" aria-labelledby="list-all-list">
-                    <!-- TODO: Make List Item -->
+                    <!-- TODO: For loop List Item -->
+
+                    <!-- TODO: Modal item edit project -->
+                    <div class="modal fade pl-0" id="editProject" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <form class="auth-form" method="POST" action="">
+                                    <div class="modal-body">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        <span class="badge badge-secondary">Menunggu Pembayaran</span>
+                                        <h4>Penyelenggara Relawan COVID</h4>
+                                        <h5 class="text-danger">Rp.1.300.000</h5>
+                                        <div class="form-group">
+                                            <label for="edit-project-name">Nama Proyek</label>
+                                            <input placeholder="Masukkan nama proyek di sini" type="text" class="form-control" id="edit-project-name" aria-describedby="nameHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-category">Kategori</label>
+                                            <select class="form-control">
+                                                <option value="">Pilih opsi</option>
+                                                <option value="Seragam Putih">Seragam Putih</option>
+                                                <option value="Seragam Kantoran">Seragam Kantoran</option>
+                                                <option value="Seragam TNI">Seragam TNI</option>
+                                                <option value="Seragam Pilot">Seragam Pilot</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-order">Jumlah Pesanan</label>
+                                            <input placeholder="Masukkan jumlah pesanan di sini" type="text" class="form-control" id="edit-project-order" aria-describedby="orderHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-quotation">Jumlah Penawaran</label>
+                                            <input placeholder="Masukkan jumlah penawaran di sini" type="text" class="form-control" id="edit-project-quotation" aria-describedby="quotationHelp">
+                                            <button class="btn btn-danger mt-1">Lihat Penawaran</button>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-address">Alamat</label>
+                                            <input placeholder="Masukkan alamat di sini" type="text" class="form-control" id="edit-project-address" aria-describedby="addressHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-address">Vendor</label>
+                                            <input placeholder="Masukkan alamat di sini" type="text" class="form-control" id="edit-project-address" aria-describedby="addressHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-startDate">Mulai Pengerjaan</label>
+                                            <input placeholder="Masukkan mulai pengerjaan di sini" type="text" class="form-control" id="edit-project-startDate" aria-describedby="startDateHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-deadline">Deadline</label>
+                                            <input placeholder="Masukkan deadline di sini" type="text" class="form-control" id="edit-project-deadline" aria-describedby="deadlineHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-note">Catatan</label>
+                                            <textarea type="text" class="form-control" id="edit-project-note" aria-describedby="noteHelp" rows="3"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="edit-project-picture">Upload Gambar</label>
+                                            <div class="upload-files__container">
+                                                <div class="upload-files__wrapper">
+                                                    <input class="upload-files__input" name="project_pict_path[]" id="edit-project-picture" type="file" class="form-control @error('project_pict_path.0') is-invalid @enderror" value="{{ old('project_pict_path.0') }}" aria-describedby="pictureAddon" multiple>
+                                                    <label for="edit-project-picture" class="upload-files__add">Upload file</label>
+                                                </div>
+                                                <div class="upload-files__preview">
+                                                </div>
+                                            </div>
+                                            @error('project_pict_path.0')
+                                            <span class="invalid-feedback" role="alert">
+                                                Some files might have invalid format or more than 2,5MB.
+                                            </span>
+                                            @enderror
+                                            <small id="pictureAddon" class="form-text text-muted">Dapat pilih banyak gambar dengan menggunakan CTRL (PC) atau hold image satu per satu (HP)</small>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
+                                        <button type="button" class="btn btn-danger">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
                     <project-item name="Penyelenggara Relawan COVID" price="1.300.000" amount="13.000" quotation="13" status="1" data-toggle="modal" data-target="#editProject" css="{{ asset('css/projectItem.css') }}"></project-item>
                     <project-item name="Seragam SMAN 4 Depok" price="2.400.000" amount="20.000" quotation="11" status="2" data-toggle="modal" data-target="#editProject" css="{{ asset('css/projectItem.css') }}"></project-item>
                     <project-item name="Seragam Kantor" price="6.200.000" amount="24.000" quotation="14" startDate="2020-10-01T00:34:00Z" endDate="2020-10-30T00:00:00Z" status="3" data-toggle="modal" data-target="#editProject" css="{{ asset('css/projectItem.css') }}"></project-item>
--- resources/views/pages/userCustomerTransactionPage.blade.php
+++ resources/views/pages/userCustomerTransactionPage.blade.php
@@ -7,15 +7,14 @@
 @endsection
 
 @section('prerender-js')
-    <script src="{{ asset('js/userCustomerTransaction.js') }}"></script>
 @endsection
 
 @section('extra-css')
     <link rel="stylesheet" href="{{ asset('css/userCustomerTransaction.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/listItem.css') }}">
 @endsection
 
 @section('content')
-@include('layouts/modalTransaction')
 
 <div class="userCustomerTransaction">
     <div class="userCustomerTransaction__container">
@@ -34,42 +33,73 @@
                 <!-- Semua Transaksi -->
                 <div class="tab-pane fade show active" id="list-all" role="tabpanel" aria-labelledby="list-all-list">
                     <!-- TODO: Make List Item -->
-                    <transaction-item
-                        name="Penyelenggara Relawan COVID"
-                        preview="{{ asset('img/hero-image.jpg') }}"
-                        startDate="2020-10-03T21:20:37.613Z"
-                        endDate="2020-12-10T21:20:37.613Z"
-                        price="13.000"
-                        amount="13.000"
-                        paidStatus="BELUM_DIBAYAR"
-                        category="SAMPLE"
-                        data-toggle="modal" data-target="#editTransaction"
-                        css="{{ asset('css/transactionItem.css') }}">
-                    </transaction-item>
-                    <transaction-item
-                        name="Seragam SMAN 4 Depok"
-                        preview="{{ asset('img/hero-image.jpg') }}"
-                        startDate="2020-10-03T21:20:37.613Z"
-                        endDate="2020-12-10T21:20:37.613Z"
-                        price="20.000"
-                        amount="20.000"
-                        paidStatus="MENUNGGU_VERIFIKASI"
-                        category="DEPOSIT"
-                        data-toggle="modal" data-target="#editTransaction"
-                        css="{{ asset('css/transactionItem.css') }}">
-                    </transaction-item>
-                    <transaction-item
-                        name="Seragam Kantor"
-                        preview="{{ asset('img/hero-image.jpg') }}"
-                        startDate="2020-10-03T21:20:37.613Z"
-                        endDate="2020-12-10T21:20:37.613Z"
-                        price="24.000"
-                        amount="24.000"
-                        paidStatus="SUDAH_DIBAYAR"
-                        category="PELUNASAN"
-                        data-toggle="modal" data-target="#editTransaction"
-                        css="{{ asset('css/transactionItem.css') }}">
-                    </transaction-item>
+                    
+                    <!-- TODO: Modal Item -->
+                    <div class="modal fade pl-0" id="editTransaction" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <form class="auth-form" method="POST" action="">
+                                    <div class="modal-body">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        <span class="badge badge-secondary">Menunggu Pembayaran</span>
+                                        <h4>Penyelenggara Relawan COVID</h4>
+                                        <h5 class="text-danger">Rp.1.300.000</h5>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
+                                        <button type="button" class="btn btn-danger">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal fade pl-0" id="uploadPayment" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <form class="auth-form" method="POST" action="">
+                                    <div class="modal-body">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        <h4>Upload Bukti Pembayaran</h4>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
+                                        <button type="button" class="btn btn-danger">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="listItem">
+                        <div class="listItem--left" data-toggle="modal"
+                            data-target="#editTransaction">
+                            <div class="listItem__header">
+                                <h5 class="listItem__name mb-0">Penyelenggara Relawan COVID</h5>
+                                <p class="listItem__price">Rp.13.000</p>
+                                <p class="listItem__amount">13.000 buah</p>
+                            </div>
+                        </div>
+                        <div class="listItem--right">
+                            <div class="listItem__label" data-toggle="modal"
+                                data-target="#editTransaction">
+                                <p class="listItem__category">Sample</p>
+                                <p class="listItem__paidStatus">Belum Dibayar</p>
+                                <!-- Uncomment if paidStatus is not SUDAH DIBAYAR -->
+                                <!-- <div class="listItem__status--progress progress"><div class="progress-bar" role="progressbar" style="width: 40%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"><p>40 Hari Lagi</p></div></div> -->
+                            </div>
+                            <div class="listItem__credential">
+                                <!-- Uncomment one of element if paidStatus is BELUM DIBAYAR or SUDAH DIBAYAR  -->
+                                <!-- <button class="btn btn-outline-danger mr-0" data-toggle="modal" data-target="#uploadPayment">Unggah Bukti Pembayaran</button> -->
+                                <!-- <a href="#"><button class="btn btn-outline-danger mr-0">Unduh MOU</button></a> -->
+                                <a href="#"><button class="btn btn-outline-danger mr-0">Unduh Invoice</button></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 
                 <!-- Penawaran Terbuka -->

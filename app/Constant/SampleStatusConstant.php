1d51102bf243e04b86f95b2dd560acd1696b49fb
razaqa <dhafins18@gmail.com>
2020-12-16 17:07:38 +0700

    Add transaction and sample constant

---
+++ app/Constant/SampleStatusConstant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constant;
+
+class SampleStatusConstant
+{
+    public const SAMPLE_WAIT_PAYMENT        = 'Menunggu Pembayaran';
+    public const SAMPLE_PAYMENT_OK          = 'Pembayaran Berhasil';
+    public const SAMPLE_WORK_IN_PROGRESS    = 'Sampel Dikerjakan';
+    public const SAMPLE_FINISHED            = 'Sampel Selesai';
+    public const SAMPLE_SENT                = 'Sampel Dikirim';
+    public const SAMPLE_APPROVED            = 'Sampel Disetujui';
+}

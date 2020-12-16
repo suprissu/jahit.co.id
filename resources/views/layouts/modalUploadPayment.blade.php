874ec5b0488d607fe6b190ee0353826831416864
razaqa <dhafins18@gmail.com>
2020-12-16 17:09:29 +0700

    Modify modal for transaction, sample, and payment slip

--- resources/views/layouts/modalUploadPayment.blade.php
+++ resources/views/layouts/modalUploadPayment.blade.php
@@ -1,15 +1,17 @@
 <div class="modal fade pl-0" id="uploadPayment" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <form class="auth-form" method="POST" action="">
+            <form id="upload-payment-form" class="auth-form" method="POST" action="">
                 <div class="modal-body">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
-                    <span id="payment-id-text" class="badge badge-danger"></span>
-                    <h4 id="payment-name-text"></h4>
-                    <h5 class="text-danger">Rp.<span id="payment-price-text"></span></h5>
-                    <input name="payment-id" id="payment-id" style="display: none;"/>
+                    <span class="payment-id-text badge badge-danger"></span>
+                    <h5 class="text-danger">Rp.<span class="payment-price-text"></span></h5>
+
+                    <input name="transactionID" class="transaction-id" type="text" style="display: none;" required>
+                    <input name="deadline" class="deadline" type="text" style="display: none;" required>
+                    
                     <div class="form-group">
                         <label for="payment-pict">Upload Bukti Pembayaran</label>
                         <div class="input-group">
@@ -18,7 +20,7 @@
                             </div>
                             <div class="input-files">
                                 <p class="input-files-filename">{{ old('payment-pict_link') }}</p>
-                                <input placeholder="payment-pict_ahmadsupriyanto.jpg" name="payment-pict_link" id="payment-pict" type="file" class="form-control @error('payment-pict_link') is-invalid @enderror" value="{{ old('payment-pict_link') }}" aria-describedby="payment-pictAddon" required>
+                                <input placeholder="payment-pict_ahmadsupriyanto.jpg" name="payment_slip_path" id="payment-pict" type="file" class="form-control @error('payment-pict_link') is-invalid @enderror" value="{{ old('payment-pict_link') }}" aria-describedby="payment-pictAddon" required>
                             </div>
                         </div>
                         @error('payment-pict_link')
@@ -30,7 +32,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-danger">Submit</button>
+                    <button type="submit" class="btn btn-danger">Submit</button>
                 </div>
             </form>
         </div>

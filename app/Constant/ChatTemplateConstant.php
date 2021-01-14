579340e87db555760d08ab47cbc5677e536e5865
razaqa <dhafins18@gmail.com>
2021-01-15 02:52:38 +0700

    Add chat template constant

--- app/Constant/ChatTemplateConstant.php
+++ app/Constant/ChatTemplateConstant.php
@@ -6,6 +6,7 @@ class ChatTemplateConstant
 {
     public const CUSTOMER_ROLE              = 'CLIENT';
     public const PARTNER_ROLE               = 'VENDOR';
+    public const ADMIN_ROLE                 = 'ADMIN';
 
     public const INITIATION_TYPE            = 'INISIASI';
     public const NEGOTIATION_TYPE           = 'NEGOSIASI';
@@ -13,8 +14,10 @@ class ChatTemplateConstant
     public const PROJECT_OFFER_TYPE         = 'DIAJUKAN';
     public const SAMPLE_REQUEST_TYPE        = 'SAMPLE';
     public const VERIFICATION_TYPE          = 'VERIFIKASI';
+    public const VERIFICATION_REJECTED_TYPE = 'VERIFIKASI DITOLAK';
     public const SAMPLE_SENT_TYPE           = 'SAMPLE TERKIRIM';
     public const PROJECT_DEAL_TYPE          = 'DEAL';
+    public const PROJECT_FINISH_TYPE        = 'FINISH';
     public const REVISION_REJECTED_TYPE     = 'REVISI DITOLAK';
 
     public const BLANK_ANSWER               = "";

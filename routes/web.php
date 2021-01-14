6c143492229f147b03c64be685ad285a443ee335
razaqa <dhafins18@gmail.com>
2021-01-15 02:51:26 +0700

    Change route for project

--- routes/web.php
+++ routes/web.php
@@ -50,10 +50,10 @@ Route::group(['prefix' => 'register', 'as' => 'register.'], function () {
 Route::group(['prefix' => 'home', 'as' => 'home'], function () {
     Route::get('', 'HomeController@index')->name('');
 
-    Route::group(['prefix' => 'customer', 'as' => '.customer'], function () {
-        Route::group(['prefix' => 'project', 'as' => '.project'], function () {
-            Route::post('/add', 'ProjectController@store')->name('.add');
-            Route::post('/edit', 'ProjectController@update')->name('.edit');
+    Route::group(['prefix' => 'project', 'as' => '.project'], function () {
+        Route::post('/add', 'ProjectController@store')->name('.add');
+        Route::post('/edit', 'ProjectController@update')->name('.edit');
+    });
 
     Route::group(['prefix' => 'administrator', 'as' => '.administrator'], function () {
         Route::group(['prefix' => 'verification', 'as' => '.verification'], function () {

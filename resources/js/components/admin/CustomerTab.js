3693c40d5760941a43312bed8b10acc595780ba9
butterchoco <ahmadsupri1999@gmail.com>
2021-03-17 19:49:41 +0700

    add filter all project admin dashboard

--- resources/js/components/admin/CustomerTab.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
-
-const CustomerTab = ({ data }) => {
-    return <Box padding={5} marginY={2} shadow="md" borderWidth="1px"></Box>;
-};
-
-export default CustomerTab;
